ab76da0a6fd41a5d26d1d1f20af2268bc1085bda
chosj <asdfzxc5353@gmail.com>
2024-11-04 22:46:53 +0900

    Json 데이터 출력하는 방법 가이드 / 11-04 일자 숙제 추가

--- src/main/java/org/example/mygradle01/controller/HomeController.java
+++ src/main/java/org/example/mygradle01/controller/HomeController.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import org.example.mygradle01.entity.Home;
 import org.example.mygradle01.service.HomeService;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,9 +28,22 @@ public class HomeController {
     }
 
     // JSON 형식으로 모든 Home을 반환하는 메서드
-    @GetMapping ("/home") // 기본 경로에 대한 GET 요청
-    public List<Home> getAllHomes(){
-        return homeService.getAllHomes(); // HomeService를 통해 모든 HomeDto를 가져옴
+//    @GetMapping ("/home") // 기본 경로에 대한 GET 요청
+//    public List<Home> getAllHomes(){
+//        return homeService.getAllHomes(); // HomeService를 통해 모든 HomeDto를 가져옴
+//    }
+    // Json으로 리턴하고 싶으면 이렇게 JsonObject와 JsonArray를 사용해서 Json객체에 데이터를 담아 리턴해주어야 하겠지요 ?
+    // 위에 처럼 데이터만 넘겨준다고 JSON 형식으로 절대 뿌려주지 않습니다~ 내가 이렇게 넘겨준다고 다 명시 해주어야지요.
+    @GetMapping("/home")
+    public String getAllHomes() throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
+        List<Home> list = homeService.getAllHomes();
+        for(int i = 0; i < list.size(); i++) {
+            jsonArray.put(list.get(i));
+        }
+        jsonObject.put("homes", jsonArray);
+        return jsonObject.toString();
     }
 
     // 새로운 Home 항목을 추가하는 메서드
